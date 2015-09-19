ecd59ede4aabe63c7267e62a94d3506cd3f011d9
das2416 <davestudin777@gmail.com>
2015-09-19 07:38:35 -0400

    added new scripts to link buttons

--- HobbyExpenditureCalculator/Content/site.js
+++ HobbyExpenditureCalculator/Content/site.js
@@ -1,9 +1,72 @@
-﻿function swapDivDisplayed(elements) {
-    var formParts = document.getElementsByClassName('form-part');
-    for (var i = 0; i < formParts.length; i++) {
-        formParts[i].style.display = 'none';
+﻿
+function showUserInfo1() {
+    var nextPartial = document.getElementById('userinfo1');
+    if (nextPartial.style.display == 'none') {
+        document.getElementById('userinfo1').style.display = 'block'
+        document.getElementById('userinfo2').style.display = 'none'
+        document.getElementById('Rent').style.display = 'none'
+        document.getElementById('Buy').style.display = 'none'
+        document.getElementById('Hobbies').style.display = 'none'
+        document.getElementById('Results').style.display = 'none'
     }
-    for (var i = 0; i < elements; i++) {
-        elements[i].style.display = 'block'
+}
+
+function showUserInfo2() {
+    var nextPartial = document.getElementById('userinfo2');
+    if (nextPartial.style.display == 'none') {
+        document.getElementById('userinfo1').style.display = 'none'
+        document.getElementById('userinfo2').style.display = 'block'
+        document.getElementById('Rent').style.display = 'none'
+        document.getElementById('Buy').style.display = 'none'
+        document.getElementById('Hobbies').style.display = 'none'
+        document.getElementById('Results').style.display = 'none'
+    }
+}
+
+function showRent() {
+    var nextPartial = document.getElementById('Rent');
+    if (nextPartial.style.display == 'none') {
+        document.getElementById('userinfo1').style.display = 'none'
+        document.getElementById('userinfo2').style.display = 'none'
+        document.getElementById('Rent').style.display = 'block'
+        document.getElementById('Buy').style.display = 'none'
+        document.getElementById('Hobbies').style.display = 'none'
+        document.getElementById('Results').style.display = 'none'
+    }
+}
+
+function showBuy() {
+    var nextPartial = document.getElementById('Buy');
+    if (nextPartial.style.display == 'none') {
+        document.getElementById('userinfo1').style.display = 'none'
+        document.getElementById('userinfo2').style.display = 'none'
+        document.getElementById('Rent').style.display = 'none'
+        document.getElementById('Buy').style.display = 'Buy'
+        document.getElementById('Hobbies').style.display = 'none'
+        document.getElementById('Results').style.display = 'none'
+    }
+}
+
+function showHobbie() {
+    var nextPartial = document.getElementById('Hobbies');
+    if (nextPartial.style.display == 'none') {
+        document.getElementById('userinfo1').style.display = 'none'
+        document.getElementById('userinfo2').style.display = 'none'
+        document.getElementById('Rent').style.display = 'none'
+        document.getElementById('Buy').style.display = 'none'
+        document.getElementById('Hobbies').style.display = 'block'
+        document.getElementById('Results').style.display = 'none'
+    }
+}
+
+function showResults() {
+    var nextPartial = document.getElementById('Results');
+    if (nextPartial.style.display == 'none') {
+        document.getElementById('userinfo1').style.display = 'none'
+        document.getElementById('userinfo2').style.display = 'none'
+        document.getElementById('Rent').style.display = 'none'
+        document.getElementById('Buy').style.display = 'none'
+        document.getElementById('Hobbies').style.display = 'none'
+        document.getElementById('Results').style.display = 'block'
     }
 }
