d5f75cc141f6bdbfa5a5c5a03c01bbc7ec5eebaf
Paul Darragh <pmd6624@rit.edu>
2015-09-19 01:47:40 -0400

    Add Some Hobbies Stuff
    Still working on making it look better

---
+++ HobbyExpenditureCalculator/Controllers/HobbiesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HobbyExpenditureCalculator.Controllers
+{
+    public class HobbiesController : Controller
+    {
+        // GET: Hobbies
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
